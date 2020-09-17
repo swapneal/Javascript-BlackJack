64225b16a90741d579a82c2dd0ae423258e9a3cf
swapneal <52267392+swapneal@users.noreply.github.com>
2020-09-17 14:33:54 -0700

    js addded

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+function blackjackHit() {
+	alert('Hit');
+}
+document.querySelector('#blackjack-stand-button').addEventListener('click', blackjackStand);
+function blackjackStand() {
+	alert('Stand');
+}
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+function blackjackDeal() {
+	alert('Deal');
+}
